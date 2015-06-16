960d085b307fcab90cc2caaab21a2f9905259658
Žilvinas Kuusas <zilvinas@kuusas.lt>
2015-06-16 10:06:17 +0300

    Initial commit: upload file and create signing examples

---
+++ create-signing.php
@@ -0,0 +1,104 @@
+<?php
+require_once __DIR__ . '/config.php';
+require_once __DIR__ . '/lib.php';
+
+/**
+ * 
+ * PARAMETERS
+ * 
+ */
+
+$signers = [];
+$files = [];
+
+/**
+ * 
+ * CHANGE THIS
+ * 
+ * File token provided by file upload response.
+ */
+$file['token'] = '557fc8bcef721a50edb61f4bbdce166b752dbd3d3c434fb2de1ab';
+
+array_push($files, $file); // For 'pdf' type only one file is supported.
+
+/**
+ * Signing type. Only 'pdf' is supported at the moment. See documentation.
+ */
+$type = 'pdf';
+
+/**
+ * Signing name. Will be displayed as the main title.
+ */
+$signingName = 'Agreement';
+
+/**
+ * Signer's unique identifier - personal code.
+ */
+$signerUID = '51001091072';
+$signer['id'] = $signerUID;
+
+/**
+ * Name
+ */
+$signer['name'] = 'Tester';
+
+/**
+ * Surname
+ */
+$signer['surname'] = 'Surname';
+
+/**
+ * Phone number. Optional.
+ */
+$signer['phone'] = '+37260000007';
+
+/**
+ * Personal code. Optional.
+ */
+$signer['code'] = '51001091072';
+
+/**
+ * Signing purpose. Availabe options listed in documentation.
+ */
+$signer['signing_purpose'] = 'signature';
+
+array_push($signers, $signer); // Add as many signers as you need.
+
+
+/**
+ * 
+ * MAKING API REQUESTS
+ * 
+ */
+
+/**
+ * Create signing
+ */
+$action = 'signing/create';
+$createResponse = request(getApiUrlByAction($action), [
+    'type' => $type,
+    'name' => $signingName,
+    'signers' => $signers,
+    'files' => $files,
+], REQUEST_POST);
+
+if ($createResponse['status'] != 'ok') {
+    echo "Signing could not be created." . PHP_EOL;
+    exit;
+}
+
+/**
+ * Important!
+ * Signing URL formation.
+ */
+$signingUrl = trim($apiUrl, '/') . "/signing/" . $createResponse['token'] . '?access_token=' . $createResponse['signers'][$signerUID];
+
+echo "Signing successfully created." . PHP_EOL;
+echo "View and sign it here: " . PHP_EOL . PHP_EOL;
+echo $signingUrl . PHP_EOL . PHP_EOL;
+
+echo "Signing url formation: " . trim($apiUrl, '/') . "/signing/<SIGNING_TOKEN>?access_token=<SIGNER_ACCESS_TOKEN>" . PHP_EOL;
+echo "SIGNING_TOKEN: token received with 'signing/create' API call response." . PHP_EOL;
+echo "SIGNER_ACCESS_TOKEN: token received with 'signing/create' API call response as parameter 'signers'.
+Signers represented as associative array where key is signer's unique identifier - personal code." . PHP_EOL;
+
---
+++ upload-file.php
@@ -0,0 +1,74 @@
+<?php
+require_once __DIR__ . '/config.php';
+require_once __DIR__ . '/lib.php';
+
+/**
+ * 
+ * PARAMETERS
+ * 
+ */
+
+/**
+ * File name of document you want to sign.
+ */
+$file['name'] = 'test.pdf'; 
+
+/**
+ * HTTP URL where the file is stored.
+ * Gateway API will download the file from given resource URL.
+ * Ensure that file URL is accessible from internet.
+ */
+$file['url'] = 'http://developers.isign.io/sc/test.pdf';
+
+/**
+ * SHA1 digest of file content.
+ */
+$file['digest'] = 'a50edb61f4bbdce166b752dbd3d3c434fb2de1ab';
+
+
+/**
+ * 
+ * MAKING API REQUESTS
+ * 
+ */
+
+/**
+ * Upload file
+ */
+$action = 'upload';
+$uploadResponse = request(getApiUrlByAction($action), [
+    'file' => $file
+], REQUEST_POST);
+
+if ($uploadResponse['status'] != 'ok') {
+    echo "File could not be uploaded.
+Please ensure that file URL is accessible from the internet." . PHP_EOL;
+    exit;
+}
+
+/**
+ * Check file status
+ */
+$action = 'upload/status/' . $uploadResponse['token'];
+$statusResponse = '';
+while ($statusResponse === '' || $statusResponse['status'] == 'pending') {
+    $statusResponse = request(getApiUrlByAction($action), [
+        'token' => $uploadResponse['token']
+    ], REQUEST_GET);
+    sleep(2);
+}
+
+if (empty($statusResponse) || $statusResponse['status'] != 'uploaded') {
+    echo "Gateway API could not download the file.
+Please ensure that file URL is accessible from the internet." 
+        . PHP_EOL;
+    exit;
+}
+
+/**
+ * Success. Signing can be created using file token.
+ */
+echo "File has been successfully uploaded." . PHP_EOL;
+echo "File token is: " . PHP_EOL . PHP_EOL;
+echo "     "  . $uploadResponse['token'] . PHP_EOL . PHP_EOL;
+echo "Use this token to create signing." . PHP_EOL;

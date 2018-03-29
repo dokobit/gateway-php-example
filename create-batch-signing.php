7fd143a06b0ea35264170f274efc2cdd5ce3118a
Marius Blazaitis <marius.b@estina.lt>
2018-03-29 11:10:20 +0300

    script for creating batch signing

---
+++ create-batch-signing.php
@@ -0,0 +1,124 @@
+<?php
+require_once __DIR__ . '/config.php';
+require_once __DIR__ . '/lib.php';
+
+/**
+ * 
+ * PARAMETERS
+ * 
+ */
+
+$signers = [];
+$files = [];
+
+/**
+ * 
+ * CHANGE THIS
+ * 
+ * File token provided by file upload response.
+ */
+
+$file['token'] = '45f943fcc30399e2ec4ea7d3473c5c52105ea3e2';
+
+array_push($files, $file); // For 'pdf' type only one file is supported.
+
+/**
+ * Signed document format. Check documentation for all available options.
+ */
+$type = 'pdf';
+
+/**
+ * Signing name. Will be displayed as the main title.
+ */
+$signingName = 'Agreement';
+
+/**
+ * Signer's unique identifier - personal code.
+ */
+$signerUID = '51001091072';
+$signer['id'] = $signerUID;
+
+/**
+ * Name
+ */
+$signer['name'] = 'Tester';
+
+/**
+ * Surname
+ */
+$signer['surname'] = 'Surname';
+
+/**
+ * Phone number. Optional.
+ */
+$signer['phone'] = '+37260000007';
+
+/**
+ * Personal code. Optional.
+ */
+$signer['code'] = '51001091072';
+
+/**
+ * Signing purpose. Availabe options listed in documentation.
+ */
+$signer['signing_purpose'] = 'signature';
+
+array_push($signers, $signer); // Add as many signers as you need.
+
+
+/**
+ * 
+ * MAKING API REQUESTS
+ * 
+ */
+
+/**
+ * Create multiple signings
+ */
+$action = 'signing/create';
+
+$signings = [];
+for ($i = 1 ; $i <= 2; $i++) {
+	$createResponse = request(getApiUrlByAction($action), [
+	    'type' => $type,
+	    'name' => $signingName,
+	    'signers' => $signers,
+	    'files' => $files,
+	    'postback_url' => $postbackUrl,
+	], REQUEST_POST);
+
+	if ($createResponse['status'] != 'ok') {
+	    echo "Signing could not be created." . PHP_EOL;
+	    exit;
+	}
+
+	$signings[] = [
+		'token' => $createResponse['token'],
+        'signer_token' => $createResponse['signers']['51001091072'],
+	];
+}
+
+$action = 'signing/createbatch';
+$batchSigningCreateResponse = request(getApiUrlByAction($action), [
+	'signings' => $signings
+]);
+
+
+/**
+ * Important!
+ * Signing URL formation.
+ */
+$batchSigningUrl = trim($apiUrl, '/') . "/signing/batch/" . $batchSigningCreateResponse['token'];
+$sequentialSigningUrl = trim($apiUrl, '/') . "/signing/sequence/" . $batchSigningCreateResponse['token'];
+
+echo "Batch signing successfully created." . PHP_EOL;
+echo "View and sign it here: " . PHP_EOL . PHP_EOL;
+echo $batchSigningUrl . PHP_EOL . PHP_EOL . PHP_EOL;
+
+echo "Sequential signing successfully created as well." . PHP_EOL;
+echo "View and sign it here: " . PHP_EOL . PHP_EOL;
+echo $sequentialSigningUrl . PHP_EOL . PHP_EOL;
+
+echo "Signing url formation: " . trim($apiUrl, '/') . "/signing/batch/<BATCH_SIGNING_TOKEN> or " . trim($apiUrl, '/') . "/signing/sequence/<BATCH_SIGNING_TOKEN>"  . PHP_EOL;
+echo "SIGNING_TOKEN: token received with 'signing/createbatch' API call response." . PHP_EOL;
+

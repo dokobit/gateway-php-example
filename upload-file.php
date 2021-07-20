6ea4888d884b8da01beb5f3c012c1215b0406cb9
Edgaras Pelanis <70586563+epelanis@users.noreply.github.com>
2021-07-20 12:02:41 +0300

    Update upload-file.php

--- upload-file.php
+++ upload-file.php
@@ -15,15 +15,15 @@ $file['name'] = 'test.pdf';
 
 /**
  * HTTP URL where the file is stored.
- * Gateway API will download the file from given resource URL. Ensure that file URL is accessible from internet.
+ * Documents Gateway will download the file from given resource URL. Ensure that file URL is accessible from internet.
  * Base64 encoded file[content] could be used instead of file[url].
  */
-$file['url'] = 'https://developers.dokobit.com/sc/test.pdf';
+$file['url'] = 'https://gateway-sandbox.dokobit.com/sc/test.pdf';
 
 /**
  * SHA256 file hash. SHA1 is supported, but not recommended.
  */
-$file['digest'] = 'a50edb61f4bbdce166b752dbd3d3c434fb2de1ab';
+$file['digest'] = '1d879e6248e4dc553b286fc5f81dc52bf841ead031287228548551a70cf06560';
 
 
 /**
@@ -59,7 +59,7 @@ while ($statusResponse === '' || $statusResponse['status'] == 'pending') {
 }
 
 if (empty($statusResponse) || $statusResponse['status'] != 'uploaded') {
-    echo "Gateway API could not download the file.
+    echo "Documents Gateway could not download the file.
 Please ensure that file URL is accessible from the internet." 
         . PHP_EOL;
     exit;
